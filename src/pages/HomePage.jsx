0c597c038f26f3b788e5472c6fc04b8046d0b823
ayusudi <ayusudi.abc@gmail.com>
2024-08-20 12:47:31 +0700

    add animation bullet

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import hangout from "../assets/hangout.png";
+import pink from "../assets/bulletpink.png"
+import blue from "../assets/bulletblue.png"
+import floor from "../assets/floor.png"
 import { Link, useNavigate } from "react-router-dom";
 import { useGoogleLogin } from '@react-oauth/google';
 import axios from "axios";
@@ -52,25 +55,47 @@ const HangoutAI = () => {
           Plan your next adventure effortlessly with Hangout AI.<br />Tailor your trips to perfection using our intelligent travel assistant.
         </p>
       </div>
+      <div className="hidden md:flex ">
+        <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
+          <div className="circle bg-blue-500" style={{ top: "20%", left: "10%" }}>
+            <img src={blue} alt="blue1" />
+          </div>
+          <div className="circle bg-pink-500" style={{ top: "30%", right: "10%" }}>
+            <img src={pink} alt="pink1" />
+          </div>
+          <div className="circle bg-blue-500" style={{ bottom: "20%", left: "15%" }}>
+            <img src={blue} alt="blue2" />
+          </div>
+          <div className="circle bg-pink-500" style={{ bottom: "10%", right: "15%" }}>
+            <img src={pink} alt="pink2" />
+          </div>
+        </div>
+
+      </div>
 
       {/* Globe Image */}
-      <div className="mt-12 relative flex justify-center items-center md:size-[500px] sm:size-[250px]">
-        <div className="absolute w-80 h-80 border-4 border-purple-400 rounded-full animate-pulse">
-        </div>
-        <div className="bg-gradient-to-r from-[#B85CA7] to-[#52EDF2] absolute md:size-[500px] sm:size-[250px] border-4 border-blue-500 rounded-full animate-spin-slow">
+      <div>
+        <div className="z-10 mt-12 relative flex justify-center items-center md:size-[500px] sm:size-[250px]">
+          <div className="absolute w-80 h-80 border-4 border-purple-400 rounded-full animate-pulse">
+          </div>
+          <div className="bg-gradient-to-r from-[#B85CA7] to-[#52EDF2] absolute md:size-[500px] sm:size-[250px] border-4 border-blue-500 rounded-full animate-spin-slow">
+          </div>
+          <img
+            src={hangout}
+            alt="Globe"
+            className="max-w-[90%] relative sm:size-[150px] md:size-[400px] rounded-full"
+          />
+
         </div>
         <img
-          src={hangout}
+          src={floor}
           alt="Globe"
-          className="max-w-[90%] relative sm:size-[150px] md:size-[400px] rounded-full"
+          className="left-0 bottom-[-100px] absolute"
         />
       </div>
 
-      {/* Video Embed Placeholder */}
-
-
       {/* Additional Sections */}
-      <div className="flex flex-col gap-6 my-10 sm:w-[90%] md:w-[640px]">
+      <div className="flex flex-col gap-6 my-20 md:my-36 sm:w-[90%] md:w-[640px]">
         <div id="aboutus" className="border-gradient h-40">
           <p className="text-gray-300">DETAIL FEATURE</p>
         </div>
