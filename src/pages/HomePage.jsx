4d962f8eee90db96c271bf1bf3f0e44cbf472dc4
ayusudi <ayusudi.abc@gmail.com>
2024-08-21 10:58:25 +0700

    update navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,7 +24,7 @@ const HangoutAI = () => {
       localStorage.setItem("image", user.image)
       localStorage.setItem("createdAt", user.createdAt)
       setLoading(false)
-      navigate("/chat");
+      navigate("/chats");
     },
     onError: error => {
     },
