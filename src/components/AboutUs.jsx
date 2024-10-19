25915014eea6fcb1a9288ea7826520746905e157
Ayu Sudi <ayusudi.abc@gmail.com>
2024-10-20 03:17:55 +0700

    PWA Hangout AI and button change chat for mobile

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,16 +1,26 @@
 export default function Component() {
   return (
-    <div id="aboutus" className="border-gradient">
+    <div id="aboutus" className="border-gradient 2xl:mt-8 lg:mt-0">
       <div className="px-3 py-4">
-        <p className="text-xl text-white font-conthrax mb-4">About Hangout AI</p>
+        <p className="text-xl text-white font-conthrax mb-4">
+          About Hangout AI
+        </p>
         <p className="text-white font-myriad mb-1 text-justify">
-          We at Hangout AI are passionate about helping you create unforgettable experiences using advanced AI technology to deliver personalized itineraries tailored to your preferences.
-          <br />  <br />
-          We understand that every outing—whether for a group, a couple, or even some well-deserved "me time"—should be special. That's why we've meticulously designed our system to provide you with the most relevant and engaging recommendations.
+          We at Hangout AI are passionate about helping you create unforgettable
+          experiences using advanced AI technology to deliver personalized
+          itineraries tailored to your preferences.
           <br /> <br />
-          Our mission is to simplify the planning process, allowing you to focus on making memories. By combining cutting-edge AI, data processing, and user-friendly interfaces, we're dedicated to helping you create moments that truly matter.
+          We understand that every outing—whether for a group, a couple, or even
+          some well-deserved "me time"—should be special. That's why we've
+          meticulously designed our system to provide you with the most relevant
+          and engaging recommendations.
+          <br /> <br />
+          Our mission is to simplify the planning process, allowing you to focus
+          on making memories. By combining cutting-edge AI, data processing, and
+          user-friendly interfaces, we're dedicated to helping you create
+          moments that truly matter.
         </p>
       </div>
     </div>
-  )
+  );
 }
